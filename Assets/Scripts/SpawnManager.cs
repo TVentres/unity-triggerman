31612091d0ea71a078c5a465c1ffb31c6279729a
EliRoman <eli.j.roman@gmail.com>
2024-04-23 19:20:26 -0400

    attemps at spawner improvement

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,10 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public class MultiArrays : MonoBehaviour
+{
+    public WaveStruct[] numbers = new WaveStruct[2];
+}
+
+//Wave struct. Each wave has an array of objects that will be spawned in that wave.
+[System.Serializable]
+public struct WaveStruct
+{
+    //public GameObject[] enemyList = GameObject[1];
+    public int[] moreNumbers;
+}
+
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemy;
     public Transform[] spawnPlanes = new Transform[4];
+    
+    //keeps track of how many enamies will be in each wave. ie enemyCountPerWave[0] gives the number of enemies for wave 0
+    public int[] enemyCountPerWave = { 1, 2, 3, 4 };
+    public int currentWave=0;
+    //public int[] numbers = { new int[] { 1, 2, 3, 4 }, new int[] { 1, 2, 3, 4, 5 } };
 
     // Update is called once per frame
     void Update()
