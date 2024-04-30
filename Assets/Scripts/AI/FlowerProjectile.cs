2557f156ae7a78a056a41e6f6ad12b91025ecde3
Zezir <toddrob2002@gmail.com>
2024-04-29 20:20:00 -0400

    Added HealthPickup

--- Assets/Scripts/AI/FlowerProjectile.cs
+++ Assets/Scripts/AI/FlowerProjectile.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class FlowerProjectile : MonoBehaviour
 {
+	public int lifeSpan = 2;
+
+	void Start()
+	{
+		Destroy(gameObject, lifeSpan);
+	}
+
 	private void OnCollisionEnter(Collision collider)
 	{
 		if (!collider.gameObject.CompareTag("Enemy"))
