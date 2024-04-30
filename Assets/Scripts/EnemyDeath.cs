bf6cba648631af3b64e95ef291e11f1bd7ac1fd7
YetsMed <166149028+YetsMed@users.noreply.github.com>
2024-04-29 21:08:31 -0400

    Update EnemyDeath.cs

--- Assets/Scripts/EnemyDeath.cs
+++ Assets/Scripts/EnemyDeath.cs
@@ -4,29 +4,18 @@ using UnityEngine;
 
 public class EnemyDeath : MonoBehaviour
 {
-    //item to be spawned on death
     public GameObject item1;
-
-    //public GameObject thisEnemy;
-    public Transform enemyPos;
-
     // Start is called before the first frame update
-    void update()
-    {
-        if(Input.GetKeyDown(KeyCode.N))
-        {
-            Die();
-        }
-    }
 
     // Update is called once per frame
     public void Die()
     {
-        //Create item1 in this position
-        GameObject obj = Instantiate(item1, transform.position, transform.rotation) as GameObject;
+        GameObject obj = Instantiate(item1, Vector3.zero, Quaternion.identity, transform) as GameObject;
         
-        //destroy the object attatched to this script (this enemy object)
-        Object.Destroy(this.gameObject);
+        obj.transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
 
+        // Now unassign the parent
+        obj.transform.parent = null;
+        Destroy(gameObject);
     }
 }
